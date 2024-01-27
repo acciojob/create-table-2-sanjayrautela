823645d57e90f0641f9a87d834842a725601c348
Sanjay Rautela <140192191+sanjayrautela@users.noreply.github.com>
2024-01-27 17:38:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 function createTable() {
-    //Write your code here
+    var rowNum = prompt("input number of rows:");
+	if(rowNum === null)return;
+
+	var colNum = prompt("input number of columns:");
+	if(colNum === null)return;
+
+	var table = document.getElementById("myTable");
+	table.innerHTML='';
+
+	for(var i=0; i<rowNum; i++){
+		var row = table.insertRow(i);
+		for(var j=0; j<colNum; j++){
+			var cell = row.insertCell(j);
+			cell.innerHTML = "row" + (i+1) + "<br>column" + (j+1);
+		}
+	}
   
 }
